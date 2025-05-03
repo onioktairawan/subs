45dc3cd1d792238d249d7d05ffbc37b17c9aa7ae
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-05-03 13:58:26 +0700

    Update and rename config.py to .env

--- config.py
+++
@@ -1,7 +0,0 @@
-API_ID = 12345678
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-MONGO_URL = "mongodb+srv://username:password@cluster.mongodb.net/teleprem"
-OWNER_ID = 123456789 
-OWNER_USERNAME = "namapenggunaowner"
-TESTIMONI_CHANNEL = "channeltestimoni"
