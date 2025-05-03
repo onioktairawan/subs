dce6489f0c9f4a20aac772498859ff9bdc6897b8
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-05-03 13:03:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-MONGO_URL = "YOUR_MONGODB_URL"
-OWNER_ID = 123456789  
-CHANNEL_ID = -1001234567890
-CHANNEL_USERNAME = "channeltestimoni"  
-OWNER_USERNAME = "usernameowner"  
+API_ID = 12345678
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+MONGO_URL = "mongodb+srv://username:password@cluster.mongodb.net/teleprem"
+OWNER_ID = 123456789 
+OWNER_USERNAME = "namapenggunaowner"
+TESTIMONI_CHANNEL = "channeltestimoni"
